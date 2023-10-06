dabcf97b8d9c2aa5bebfd4e98ad2df4be77d8e54
Kyow <33874701+kyowChaing@users.noreply.github.com>
2023-10-06 08:53:32 -0800

    Add files via upload

---
+++ exporting_Data from_external/index.js
@@ -0,0 +1,70 @@
+// import data from data.txt when ocoure button clik event and render it into web page using AJAX
+
+// first creat button event listener
+
+// document.getElementById('get_data').addEventListener('click',loadData);
+
+// function loadData(){
+//     console.log("hi");
+//     // create XHR Object (Xml Http Request)
+//     let xhr = new XMLHttpRequest();
+//     // Open port
+//     xhr.open('GET','data.txt',true);
+    
+//     xhr.onprogress=function () {
+//         console.log(xhr.readyState);
+//     }
+     
+//     xhr.onload=function(){
+//         if(this.status===200){
+//             document.getElementById('output').innerHTML= '<h4> ${this.responseText} </h4>'
+//         }
+//     }
+   
+// }
+
+// script.js
+// Asynchronous Programming
+// AJAX = Asynchronous JavaScript And XML
+// Get data without loading the page
+
+document.getElementById('get_data').addEventListener('click', loadData);
+
+function loadData() {
+    // Create XHR Object (Xml Http Request)
+    let xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onprogress = function() {
+        console.log(xhr.readyState);
+    }
+
+    xhr.onload = function () {
+        // HTTP statuses
+        // 200: "OK"
+        // 403: "Forbidden"
+        // 404: "Not Found"
+        if (this.status === 200) {
+            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
+        }
+    }
+
+    //xhr.onreadystatechange = function () {
+    //     // readyState Values
+    //     // 0: request not initialized
+    //     // 1: server connection established
+    //     // 2: request received
+    //     // 3: processing request
+    //     // 4: request finished and response is ready
+    //     //console.log(this.readyState);
+    //     if (this.status === 200 && this.readyState === 4) {
+    //         console.log(this.responseText);
+    //     }
+    // }
+
+     xhr.send();
+
+    //console.log(xhr);
+}
